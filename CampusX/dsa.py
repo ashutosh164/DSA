dd0776b2260d6600300a2a322cb1e6f77cd0dd41
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2025-04-04 20:42:43 +0530

    ll reverse

--- CampusX/dsa.py
+++ CampusX/dsa.py
@@ -492,8 +492,19 @@ sum_odd_node(head)
 
 print('==========reverse ll containing int============')
 
+def reverse(head):
+    prev_node = None
+    curr_node = head
+    while curr_node:
+        next_node = curr_node.next
+        curr_node.next = prev_node
+        prev_node = curr_node
+        curr_node = next_node
+    head = prev_node
+    return head
 
 
+display(reverse(head))
 
 
 
