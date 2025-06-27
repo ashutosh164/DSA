fe1884f29771cf395083b199e0ea5bcd17bfe101
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2025-06-27 13:18:06 +0530

    changes

--- leetcode/2_sum.py
+++ leetcode/2_sum.py
@@ -11,15 +11,38 @@ def two_sum(arr, target):
             return [set[numb], item]
         set[value] = item
 
-
-
-
 print(two_sum(arr, target))
 
+'''2nd method'''
 
 
+def tow_sum2(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low < high:
+        new_val = arr[low] + arr[high]
+        if new_val == target:
+            return arr[low], arr[high]
+        elif new_val < target:
+            low += 1
+        else:
+            high -= 1
+    print()
+
+arr = [3, 6, 2, 8, 4, 8, 9, 6, 1, 9, 0]
+# arr = [2,1,5,7,9]
 
 
+def func(arr, target):
+    set = {}
 
+    for item, val in enumerate(arr):
+        num = target - val
+        if num in set:
+            return [set[num], val]
+        set[val] = item
+
+print(func(arr,10))
 
 
