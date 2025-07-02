45c7504eb0ec4702d1b7ac99f6303cfeb0cfddf7
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2025-07-02 08:34:44 +0530

    merge 2 ll

--- greg hoog/linkedlist.py
+++ greg hoog/linkedlist.py
@@ -103,6 +103,46 @@ traverse(head)
 
 
 
+print('=========merge two sorted ll==================')
+
+'''
+approach
+
+list1, list2
+
+dummy_ll = SingleNode()
+curr = dummy_ll
+
+while list1 and list2:
+    if list1.val < list2.val:
+        curr.next = list1
+        curr = list1
+        list1 = list1.next
+    else:
+        curr.next = list2
+        curr = list2
+        list2 = list2.next
+curr.next = list1 if list1 else list2
+return dummy_ll.next
+    
+
+'''
+
+
+def merge_two_sort_ll(list1, list2):
+    dummy_ll = SingleNode()
+    curr = dummy_ll
+    while list1 and list2:
+        if list1.val < list2.val:
+            curr.next = list1
+            curr = list1
+            list1 = list1.next
+        else:
+            curr.next = list2
+            curr = list2
+            list2 = list2.next
+    curr.next = list1 if list1 else list2
+    return dummy_ll.next
 
 
 
