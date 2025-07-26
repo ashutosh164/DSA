9a2aca91f36f8050dd0900dbde0e1e4d26d7da5f
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2025-07-26 10:51:22 +0530

    remmove tail

--- TUF/ll.py
+++ TUF/ll.py
@@ -40,6 +40,23 @@ print('====after delete head=====')
 
 a = remove_head(head)
 traverse(a)
+print('==========delete the tail==========')
+
+
+def remove_tail(head):
+    if head is None or head.next is None:
+        return None
+    temp = head
+    while temp.next.next:
+        temp = temp.next
+    del temp.next
+    temp.next = None
+    return head
+
+traverse(head)
+t = remove_tail(head)
+traverse(t)
+
 
 
 
