b488d11dc165fc5ce39815d1a97d8bb78d307d2f
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2025-08-03 11:19:38 +0530

    dp

---
+++ TUF/dp.py
@@ -0,0 +1,11 @@
+'''
+
+logest common substring
+
+'''
+
+
+
+
+
+
--- greg hoog/y_shorts.py
+++ greg hoog/y_shorts.py
@@ -18,7 +18,37 @@ def is_palindrome(string):
 
 print(is_palindrome(string))
 
+print('=========product of array except of self==========')
 
+def product_except_self(arr):
+    n = len(arr)
+    left = [0]*n
+    right = [0]*n
+    l = 1
+    r = 1
+    for i, num in enumerate(arr):
+        left[i] = l
+        j = -i-1
+        right[j] = r
+        l*= num[i]
+        r *= num[j]
+    return [l*r for l, r in zip(left, right)]
+
+
+# print(product_except_self([1,2,3,4]))
+
+print('=====majority element=========')
+
+def majority_element(arr):
+    count = 0
+    elm = None
+    for i in arr:
+        if count == 0:
+            elm = i
+        count += 1 if elm == i else -1
+    return count
+
+print(majority_element([1,1,1,2,3,4,4,4,4]))
 
 
 
