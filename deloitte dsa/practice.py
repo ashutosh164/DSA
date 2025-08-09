db3e6d3704699e7bf0a5f74a6c63c5d5c78753b3
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2025-08-09 22:54:45 +0530

    string reverse

--- deloitte dsa/practice.py
+++ deloitte dsa/practice.py
@@ -10,6 +10,37 @@ def fun(text):
 print(fun('deloitte'))
 
 
+a = 'deloitte india'
+s = ''
+for i in a:
+    s = i+s
+print(s)
+
+def isAnagram(a, b):
+    if len(a) != len(b):
+        return False
+
+    count = {}
+    for ch in a:
+        count[ch] = count.get(ch, 0) + 1
+
+    for ch in b:
+        if ch not in count:
+            return False
+        count[ch] -= 1
+        if count[ch] == 0:
+            del count[ch]
+    return len(count) == 0
+
+print(isAnagram('listen', 'silent'))
+print(isAnagram('rat', 'cat'))
+
+
+
+
+
+
+
 
 
 
