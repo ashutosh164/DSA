06a2693f8b96518e0555e807358a5247e9e7c8c3
Ashutosh pradhan <ashutoshpradhan164@gmail.com>
2026-02-12 14:02:37 +0530

    practice 3

--- deloitte dsa/practice3.py
+++ deloitte dsa/practice3.py
@@ -1,3 +1,10 @@
+from random import lognormvariate
+from selectors import SelectSelector
+
+from numpy.f2py.auxfuncs import l_not
+
+from EXL.interview import freq_count
+
 txt = 'hey,are,you,ok'
 out = 'yeh, era, uoy, ko'
 
@@ -136,7 +143,160 @@ def most_freq(arr):
 print(most_freq([1,2,3,2,4]))
 
 
+def fun(arr, target):
+    left , right = 0 , len(arr) - 1
 
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+print(fun([0,2,3,1,0,2,6,0,2,0],6))
+
+
+
+def isvalid(txt):
+    stack = []
+    maping = {')':'(','}':'{',']':'['}
+    for i in txt:
+        if i in maping:
+            top = stack.pop() if stack else '#'
+            if maping[i] != top:
+                return False
+        else:
+            stack.append(i)
+    return not stack
+
+def isPalaendrome(s):
+    s = ''.join(c.lower() for c in s if c.isalnum())
+    return s == s[::-1]
+txt = 'hey,are,you,ok'
+out = 'yeh, era, uoy, ko'
+
+res = [i[::-1] for i in txt.split(',')]
+print(', '.join(res))
+
+
+arr = [2, 4, 60, 20, 4, 90]
+
+def sencondHeighest(arr):
+    first , sec = float('-inf'), float('-inf')
+
+    for i in arr:
+        if i > first:
+            sec = first
+            first = i
+        elif i > sec and i != first:
+            sec = i
+    return sec
+print(sencondHeighest(arr))
+
+def isAnagram(a, b):
+    if len(a) != len(b):
+        return False
+    data = {}
+    for ch in a:
+        data[ch] = data.get(ch, 0 )+1
+
+    for ch in b:
+        if ch not in data:
+            return False
+        data[ch] -= 1
+        if data[ch] == 0:
+            del data[ch]
+    return len(data) == 0
+print(isAnagram('listen', 'silent'))
+print(isAnagram('rat', 'cat'))
+
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+from collections import defaultdict
+def gp_angm(words):
+    data = defaultdict(list)
+    print(data)
+    for i in words:
+        key = tuple(sorted(i))
+        print(key)
+        data[key].append(i)
+    return list(data.values())
+
+print(gp_angm(words))
+
+class LinkNode:
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+    def __str__(self):
+        return str(self.val)
+
+
+node = LinkNode(1)
+a = LinkNode(2)
+b = LinkNode(3)
+c = LinkNode(4)
+
+node.next = a
+a.next = b
+b.next = c
+
+def traverse(head):
+    curr = head
+    while curr:
+        print(curr.val, end='')
+        if curr.next:
+            print('->', end='')
+        curr = curr.next
+    print()
+
+traverse(node)
+
+def reverse(head):
+    curr = head
+    prev = None
+    while curr:
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    head = prev
+    return head
+data = reverse(node)
+traverse(data)
+
+arr = [2, 4, 60, 20, 4, 90]
+
+fst, sec = float('-inf'), float('-inf')
+for i in arr:
+    if i > fst:
+        sec = fst
+        fst = i
+    elif i > sec and i != fst:
+        sec = i
+print(sec)
+
+arr = [2, 4, 60, 20, 4, 90]
+for i in range(len(arr)):
+    for j in range(0, len(arr)-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+print(arr[-2])
+
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+def gp_anagm(word):
+    data = defaultdict(list)
+    for i in words:
+        key = tuple(sorted(i))
+        data[key].append(i)
+    return list(data.values())
+print(gp_anagm(words))
 
 
 
